c36baec4694532a46ed34d76ac575a1e93c372a6
xNicoAP <165246955+xNicoAP@users.noreply.github.com>
2024-03-28 03:16:09 -0300

    Add files via upload

---
+++ Sistema.Datos/Conexion.vb
@@ -0,0 +1,8 @@
+﻿Imports System.Data.SqlClient
+Public Class Conexion
+    Public conn As SqlConnection
+
+    Public Sub New()
+        Me.conn = New SqlConnection("Data Source=DESKTOP-QGVN1Q9;Initial Catalog=dbsistema;User ID=sa;Password=56035895")
+    End Sub
+End Class
